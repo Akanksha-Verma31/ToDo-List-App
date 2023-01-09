bf544b05d795f4c7932bef7101c8551be2bae529
Akanksha Verma <22906.akanksha@gmail.com>
2023-01-09 22:11:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { isNgTemplate } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo';
+  list:any[] = [];
+
+  addTask(item:string){
+    //console.log(item)
+    this.list.push({id:this.list.length, name:item})
+    console.warn(this.list)
+  }
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+}
